f93cc15b729e5ffbdd964ffb81c7bb0202f51593
James Richardson <3richj71@solent.ac.uk>
2019-01-28 09:59:59 +0000

    mapping

---
+++ Mapping/app/src/main/java/com/james/mapping/MainActivity.java
@@ -0,0 +1,58 @@
+package com.james.mapping;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MapView mv;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        setContentView(R.layout.activity_main);
+
+        mv = (MapView)findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(19.0);
+        mv.getController().setCenter(new GeoPoint(50.917615,-1.335753));
+        mv.setTileSource(TileSourceFactory.MAPNIK);
+
+        Button button = (Button)findViewById(R.id.goButton);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        EditText et1 = (EditText)findViewById(R.id.latitude);
+        EditText et2 = (EditText)findViewById(R.id.longitude);
+        double lon = Double.parseDouble(et1.getText().toString());
+        double lat = Double.parseDouble(et2.getText().toString());
+
+        mv.getController().setCenter(new GeoPoint(lon,lat));
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
